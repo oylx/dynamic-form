db12050f756d1f26aa6d7addf85e688512908645
oylx <oylx2008@163.com>
2022-10-26 16:57:13 +0800

    fix:拖拽

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,10 @@ export const routes = [
     path: '/',
     component: () => import('@/views/layout'),
     name: '首页',
-    redirect: '/VisualDrag'
-
+    redirect: '/VisualDrag',
+    children: [
+      { path: '', component: () => import('@/views/VisualDragIndex'), },
+    ]
   },
   {
     path: '/DynamicForm',
@@ -12,22 +14,6 @@ export const routes = [
     name: '动态表单',
     children: [
       { path: '', component: () => import('@/views/DynamicForm'), },
-    ]
-  },
-  {
-    path: '/VisualDrag',
-    component: () => import('@/views/layout'),
-    name: '拖拽组件',
-    children: [
-      { path: '', component: () => import('@/views/VisualDragIndex/index'), },
-    ]
-  },
-  {
-    path: '/VisualDrag1',
-    component: () => import('@/views/layout'),
-    name: '拖拽组件1',
-    children: [
-      { path: '', component: () => import('@/views/VisualDragIndex/index1'), },
     ]
   },
 ]
